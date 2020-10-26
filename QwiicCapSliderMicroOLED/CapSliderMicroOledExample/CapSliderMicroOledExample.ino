15af0a75bb72322ea282ecd879326b44affec68c
Marcus Stevenson <mdstevenson99@gmail.com>
2020-10-25 23:45:02 -0600

    finishing touches

--- QwiicCapSliderMicroOLED/CapSliderMicroOledExample/CapSliderMicroOledExample.ino
+++ QwiicCapSliderMicroOLED/CapSliderMicroOledExample/CapSliderMicroOledExample.ino
@@ -1,8 +1,10 @@
 /*
- * 
+ * This sketch illustrates user input from the SparkFun Capacitive Touch Slider by illuminating a bar of pixels on the Micro OLED Display to correspond with the activated touch pad. 
+ * Find the code and parts list in the GitHub repo here->
+ * https://github.com/marcus-stevenson/SFESimpleSketches/tree/master/QwiicCapSliderMicroOLED/CapSliderMicroOledExample
 */
-#include <Wire.h>  // Include Wire if you're using I2C
-#include <SFE_MicroOLED.h>  // Include the SFE_MicroOLED library
+#include <Wire.h>
+#include <SFE_MicroOLED.h>
 
 #include "bitmaps.h"
 
@@ -12,7 +14,7 @@
 #define PIN_RESET 9  
 #define DC_JUMPER 1
 
-MicroOLED oled(PIN_RESET, DC_JUMPER);    // I2C declaration
+MicroOLED oled(PIN_RESET, DC_JUMPER);
 
 CAP1203 sensor;  
 
@@ -20,31 +22,28 @@ void setup()
 {
   delay(100);
   Wire.begin();
-  oled.begin();    // Initialize the OLED
-  oled.clear(ALL); // Clear the display's internal memory
-  oled.display();  // Display what's in the buffer (splashscreen)
-  delay(1000);     // Delay 1000 ms
-  oled.clear(PAGE); // Clear the buffer.
+  oled.begin();    
+  oled.clear(ALL); 
+  oled.display();  
+  delay(1000);     
+  oled.clear(PAGE); 
 
   sensor.begin();
 }
 void loop(){
   if (sensor.isLeftTouched() == true) {
-    //Serial.println("Left");
     oled.drawBitmap(left);
     oled.display();
     delay(200);
   }
 
   if (sensor.isMiddleTouched() == true) {
-    //Serial.println("Middle");
     oled.drawBitmap(middle);
     oled.display();
     delay(200);
   }
 
   if (sensor.isRightTouched() == true) {
-    //Serial.println("Right");
     oled.drawBitmap(right);
     oled.display();
     delay(200);
